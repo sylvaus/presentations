c8a1a468fd6e9a4bd9cee55e4d80bd5450fd9315
pybreches <pybreches@cscanada.ca>
2019-11-05 17:35:14 -0500

    Started adding messages fro the user

--- python/code/project_snake/end/snake_game/snake.py
+++ python/code/project_snake/end/snake_game/snake.py
@@ -1,12 +1,15 @@
 from enum import Enum, auto
 from queue import Queue
-from time import sleep
 from typing import Tuple, Optional
 
 import pygame
 from pygame import Vector2, Surface
 from pygame.sprite import spritecollide, Group
 from pygame.time import Clock
+
+BORDER_COLOR = (153, 0, 255)
+
+WHITE = (255, 255, 255)
 
 BLACK = (0, 0, 0)
 
@@ -68,7 +71,7 @@ class Block(pygame.sprite.Sprite):
 
 class Apple(Block):
     COLOR = (255, 0, 0)  # red
-    APPLE_LENGTH = 10
+    APPLE_LENGTH = 16
     APPLE_SIZE = (APPLE_LENGTH, APPLE_LENGTH)
 
     def __init__(self, center: Vector2, *groups):
@@ -88,10 +91,10 @@ class Snake:
             position = start_position + index * DIRECTION_VECTOR_MAP[direction] * self.BLOCK_LENGTH
             # Handle head differently
             if index != (length - 1):
-                block = Block(self.BLOCK_SIZE, position, (255, 255, 255), self.body)
+                block = Block(self.BLOCK_SIZE, position, WHITE, self.body)
                 self.queue.put(block)
             else:
-                block = Block(self.BLOCK_SIZE, position, (255, 255, 255))
+                block = Block(self.BLOCK_SIZE, position, WHITE)
                 self.queue.put(block)
                 self.head = block
 
@@ -114,7 +117,7 @@ class Snake:
         self.body.add(self.head)
         # Creating head block.
         position = self.head.center + DIRECTION_VECTOR_MAP[direction] * self.BLOCK_LENGTH
-        block = Block(self.BLOCK_SIZE, position, (255, 255, 255))
+        block = Block(self.BLOCK_SIZE, position, WHITE)
         # Adding head block to snake
         self.queue.put(block)
         self.head = block
@@ -122,19 +125,38 @@ class Snake:
 
 def get_direction():
     direction = None
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit(0)
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                direction = Directions.TOP
-            if event.key == pygame.K_DOWN:
-                direction = Directions.BOTTOM
-            if event.key == pygame.K_LEFT:
-                direction = Directions.LEFT
-            if event.key == pygame.K_RIGHT:
-                direction = Directions.RIGHT
+    for event in pygame.event.get(pygame.KEYDOWN):
+        if event.key == pygame.K_UP:
+            direction = Directions.TOP
+        if event.key == pygame.K_DOWN:
+            direction = Directions.BOTTOM
+        if event.key == pygame.K_LEFT:
+            direction = Directions.LEFT
+        if event.key == pygame.K_RIGHT:
+            direction = Directions.RIGHT
     return direction
+
+
+def get_exit():
+    if pygame.event.get(pygame.QUIT):
+        exit(0)
+
+
+def write_text(
+        text: str, center: Tuple[int, int]
+        , screen: Surface
+        , font_name: str = "freesansbold.ttf"
+        , font_size: int = 25
+        , color: Tuple[int, int, int] = (255, 255, 255)
+        ,
+):
+    font = pygame.font.Font(font_name, font_size)
+
+    text_render = font.render(text, True, color)
+    text_rect = text_render.get_rect()
+    text_rect.center = center
+
+    screen.blit(text_render, text_rect)
 
 
 def main():
@@ -143,30 +165,49 @@ def main():
     direction = None
     screen = create_screen(size)
     clock = Clock()
+    is_lost = False
     score = 0
-    snake = Snake(Vector2(300, 300), 12, Directions.RIGHT)
-    apple = Apple(Vector2(10, 10))
-    while True:
-        # TODO: Implement the get direction to get the user input and move the snake_game accordingly
-
+    snake = Snake(Vector2(300, 300), 2, Directions.RIGHT)
+    apple_group = Group()
+    apple = Apple(Vector2(50, 50), apple_group)
+    wall_group = Group()
+    walls = [
+        Block((size[0], 2), Vector2(size[0] // 2, 1), BORDER_COLOR, wall_group)  # TOP
+        , Block((size[0], 32), Vector2(size[0] // 2, size[1] - 16), BORDER_COLOR, wall_group)  # Bottom
+        , Block((2, size[1]), Vector2(1, size[1] // 2), BORDER_COLOR, wall_group)  # Left
+        , Block((2, size[1]), Vector2(size[0] - 1, size[1] // 2), BORDER_COLOR, wall_group)  # Right
+    ]
+    keep_tail = False
+    while not is_lost:
+        get_exit()
         direction = get_direction() or direction
 
-        # TODO: Optional Add the code to display the score
-        # The score will increase every seconds
-
         # Update
-        snake.move(direction, False)
-        print(spritecollide(snake.head, snake.body, dokill=False))
+        snake.move(direction, keep_tail)
+        if spritecollide(snake.head, snake.body, dokill=False) or spritecollide(snake.head, wall_group, dokill=False):
+            is_lost = True
+        if spritecollide(snake.head, apple_group, dokill=True):
+            keep_tail = True
+            score += 1
+        else:
+            keep_tail = False
+
         direction = None
 
         # Draw
         screen.fill(BLACK)
+        wall_group.draw(screen)
         snake.draw(screen)
-        apple.draw(screen)
-
+        apple_group.draw(screen)
+        write_text("Score: {0}".format(score), (size[0] // 2, size[1] // 2), screen)
         pygame.display.flip()
 
         clock.tick(fps)
+
+    write_text("You Lose", (size[0] // 2, size[1] // 2), screen)
+    pygame.display.flip()
+    while True:
+        get_exit()
 
 
 if __name__ == '__main__':
